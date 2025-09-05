6b63f67c636872bb39ea521bf05c93011ac9b71e
Your Name <jomerubaldo@gmail.com>
2025-09-05 23:16:52 +0800

    initial commit

--- react/src/components/Practice.jsx
+++ react/src/components/Practice.jsx
@@ -1,22 +1,26 @@
 import { useState } from 'react';
 
 const Practice = () => {
-  const [isColor, setIsColor] = useState("");
+  const [Color, setColor] = useState('');
 
   const handleRedFunction = () => {
-    setIsColor("red")
-  }
+    return setColor(<div className="bg-red-500 w-10 h-10"></div>);
+  };
 
   const handleBlueFunction = () => {
-    setIsColor("blue")
-  }
+    return setColor(<div className="bg-blue-500 w-10 h-10"></div>);
+  };
 
   return (
-    <div className='bg-white w-screen h-screen flex justify-center items-center'>
-        <div>{isColor}</div>
-      <div className='space-x-2'>
-        <button onClick={handleRedFunction} className='bg-red-400 w-10 h-10'>Red</button>
-        <button onClick={handleBlueFunction} className='bg-blue-400 w-10 h-10'>Blue</button>
+    <div className="bg-white w-screen h-screen flex justify-center items-center">
+      <div>{Color}</div>
+      <div className="space-x-2">
+        <button onClick={handleRedFunction} className="bg-red-400 w-10 h-10">
+          Red
+        </button>
+        <button onClick={handleBlueFunction} className="bg-blue-400 w-10 h-10">
+          Blue
+        </button>
       </div>
     </div>
   );
